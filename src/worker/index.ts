0ba3129fdbcf2cafdcdb6f93adc3cd7126ccb1df
Ryan <enjoywl@126.com>
2026-06-20 16:13:04 +0800

    init:fix

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -12,7 +12,7 @@ app.get("/api/matches", async (c) => {
   const date = c.req.query("date") || new Date().toISOString().slice(0, 10);
   const status = c.req.query("status") || "1";
 
-  const url = `http://scoreref.com:9080/v2/api/soccer/match/by-status?date=${date}&status=${status}`;
+  const url = `http://api.sporthing.com:8090/v2/api/soccer/match/by-status?date=${date}&status=${status}`;
 
   const resp = await c.env.VPC_SERVICE.fetch(url);
   if (!resp.ok) {
